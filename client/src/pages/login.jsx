3e0f39f000fe83a58f257db972b544757b04e123
Shahriar-Samir <shabusiness035@gmail.com>
2024-10-02 22:34:09 +0600

    add: signin with google button on login page

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Login = () => {
     return (
-        <div>
-            login
+        <div className='w-full h-[100vh] flex justify-center items-center'>
+            <button className='btn bg-primeColor text-white hover:bg-[#F33823]'>Sign Up With Gmail</button>
         </div>
     );
 };
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'primeColor': '#F33823'
+      }
+    },
   },
   plugins: [
     require('daisyui'),
